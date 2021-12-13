fec0887313ca569cd67d8fd615ac7771afb3e6b7
mark-escosura <escosuramarkse@gmail.com>
2021-12-13 14:09:08 -0800

    Second Commit: Wrote middlewares inside auth-middleware.js

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -6,8 +6,15 @@
     "message": "You shall not pass!"
   }
 */
-function restricted() {
-
+function restricted(req, res, next) {
+  // 1- it checked response for a COOKIE header
+  // 2- pulled session id from it
+  // 3- checked if live session by that id
+  if (req.session.user) {
+    next();
+  } else {
+    next({ status: 401, message: "You shall not pass!" });
+  }
 }
 
 /*
@@ -18,8 +25,12 @@ function restricted() {
     "message": "Username taken"
   }
 */
-function checkUsernameFree() {
-
+function checkUsernameFree(req, res, next) {
+  if (!req.body.username) {
+    next();
+  } else {
+    next({ status: 422, message: "Username taken" });
+  }
 }
 
 /*
@@ -30,10 +41,13 @@ function checkUsernameFree() {
     "message": "Invalid credentials"
   }
 */
-function checkUsernameExists() {
-
+function checkUsernameExists(req, res, next) {
+  if (req.body.username) {
+    next();
+  } else {
+    next({ status: 401, message: "Invalid credentials" });
+  }
 }
-
 /*
   If password is missing from req.body, or if it's 3 chars or shorter
 
@@ -42,8 +56,18 @@ function checkUsernameExists() {
     "message": "Password must be longer than 3 chars"
   }
 */
-function checkPasswordLength() {
-
+function checkPasswordLength(req, res, next) {
+  if (req.body.password.length > 3) {
+    next();
+  } else {
+    next({ status: 422, message: "Password must be longer than 3 chars" });
+  }
 }
 
 // Don't forget to add these to the `exports` object so they can be required in other modules
+module.exports = {
+  restricted,
+  checkUsernameFree,
+  checkUsernameExists,
+  checkPasswordLength,
+};
